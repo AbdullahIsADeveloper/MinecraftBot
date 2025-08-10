0fda5f8d4f26f079ae1b0cbfd302f1e667921162
Abdullah Khan <abdullahkhang389@gmail.com>
2025-08-11 09:58:13 +1000

    Removed anti-kick

--- index.js
+++ index.js
@@ -12,26 +12,6 @@ function startBot() {
     version: '1.21.4'
   });
 
-  bot.on('spawn', async () => {
-    // Find a nearby door block (wooden or iron door)
-    const doorBlock = bot.findBlock({
-      matching: (block) => block && (block.name === 'oak_door' || block.name === 'spruce_door' || block.name === 'birch_door' || block.name === 'jungle_door' || block.name === 'acacia_door' || block.name === 'dark_oak_door'),
-      maxDistance: 32
-    });
-    if (doorBlock) {
-      console.log('Door found:', doorBlock.name, 'at', doorBlock.position);
-      setInterval(async () => {
-        try {
-          await bot.activateBlock(doorBlock);
-        } catch (err) {
-          // You may want to log errors for debugging
-        }
-      }, 500);
-    } else {
-      console.log('No door found nearby.');
-    }
-  });
-
   bot.on('end', () => {
     setTimeout(() => {
       startBot();
