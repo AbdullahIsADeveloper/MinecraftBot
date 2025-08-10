f8dfe582f3315cf5747a70a5f08e05407751e6c2
Abdullah Khan <abdullahkhang389@gmail.com>
2025-08-10 18:48:31 +1000

    Added server

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mineflayer = require('mineflayer');
+const http = require('http');
+
 const bot = mineflayer.createBot({
   host: 'bemincollege.aternos.me',
   port: 26191,
@@ -13,3 +15,8 @@ bot.on('spawn', () => {
     setTimeout(() => bot.setControlState('jump', false), 100);
   }, 200);
 });
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running\n');
+}).listen(process.env.PORT || 3000);
