7343e3d893b3c431f7b34b3a0b52937d4d01dcad
Abdullah Khan <abdullahkhang389@gmail.com>
2025-08-11 09:15:32 +1000

    Fixed kicking

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const mineflayer = require('mineflayer');
 const http = require('http');
 
-const bot = mineflayer.createBot({
-  host: 'bemincollege.aternos.me',
-  port: 26191,
-  username: 'Server',
-  auth: 'offline',
-  version: '1.21.4'
-});
+let bot;
 
-bot.on('spawn', () => {
-  setInterval(() => {
-    bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 100);
-  }, 200);
-});
+function startBot() {
+  bot = mineflayer.createBot({
+    host: 'bemincollege.aternos.me',
+    port: 26191,
+    username: 'Server',
+    auth: 'offline',
+    version: '1.21.4'
+  });
+
+  bot.on('spawn', () => {
+    setInterval(() => {
+      bot.activateItem();
+    }, 200);
+  });
+
+  bot.on('end', () => {
+    setTimeout(() => {
+      startBot();
+    }, 1000);
+  });
+}
+
+startBot();
 
 http.createServer((req, res) => {
+  if (!bot.player) {
+    startBot();
+  }
   console.log("Bot is running");
+  res.end();
 }).listen(process.env.PORT || 3000);
